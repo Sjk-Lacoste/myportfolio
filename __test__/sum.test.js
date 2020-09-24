86ab0e433ab0afa9188e281d865dc1bd1e7ed7e8
TshepoMohlatlole <tshepo@turati.co.za>
2020-09-24 12:58:46 +0200

    Removed project files

--- __test__/sum.test.js
+++
@@ -1,9 +0,0 @@
-const { ExpansionPanelActions } = require("@material-ui/core");
-
-function sum(a,b) {
-    return a+b;
-}
-
-test('should add two numbers', () => {
-    expect(sum(1,3)).toBe(4);
-})
--- components/footer.js
+++
@@ -1,7 +0,0 @@
-const Footer = () => {
-    return (
-        <></>
-    );
-}
-
-export default Footer;
