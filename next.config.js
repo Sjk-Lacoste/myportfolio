55600c29e6a04193bfb3b973db5de7883cb6042f
TshepoMohlatlole <tshepo@turati.co.za>
2020-11-29 19:02:33 +0200

    Added mailchimp library for marketing

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'assets/scss')]
-    }
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, "assets/scss")],
+  },
+};
+
+module.exports = {
+  env: {
+    MAILCHIMP_ACCESS_KEY: "e07059049dc86ef56b3b57792dec4b12-us7",
+  },
+};
