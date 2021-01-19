3dc390ac89529b73873a8b1a48caa21d42d7a12c
Sjk-Lacoste <tmohlatlole@gmail.com>
2021-01-19 19:08:29 +0200

    Added apollo and graphql

--- lib/helpers/generateId.ts
+++
@@ -1,3 +0,0 @@
-export const generateId = () => {
-  return Math.floor(100000 + Math.random() * 900000);
-};
--- redux/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import counterReducer from "./counterReducer";
-import { combineReducers } from "redux";
-
-const rootReducer = combineReducers({
-  counter: counterReducer,
-});
-
-export default rootReducer;
